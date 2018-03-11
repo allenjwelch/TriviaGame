f1026a30dbedad894ffd980108e2b8e07a252dd2
Allen Welch <allenjwelch@gmail.com>
2018-03-11 17:14:05 -0400

    added different question rounds, comments, & readied sndFX

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,28 +1,32 @@
 /////////////// Variables /////////////////
 var questionArr = {
-  q1: ["question1", "answer1"], 
-  q2: ["question2", "answer2"], 
-  q3: ["question3", "answer3"], 
-  q4: ["question4", "answer4"], 
-  q5: ["question5", "answer5"], 
+  q1: ["question1", "answerA", "answerB", "answerC", "answerD"], 
+  q2: ["question2", "answerA", "answerB", "answerC", "answerD"], 
+  q3: ["question3", "answerA", "answerB", "answerC", "answerD"],  
+  q4: ["question4", "answerA", "answerB", "answerC", "answerD"],  
+  q5: ["question5", "answerA", "answerB", "answerC", "answerD"], 
 }; 
 
-var questionCycle = [
-  questionArr.q1, 
-  questionArr.q2, 
-  questionArr.q3, 
-  questionArr.q4
-];
+// var questionCycle = [
+//   questionArr.q1, 
+//   questionArr.q2, 
+//   questionArr.q3, 
+//   questionArr.q4
+// ];
 
 var timerOn = false; 
 var interval; 
 var time = 10; 
-// var questionSet; 
+var correct = false; // ---- Will need to set to true when correct answer is chosen then reset back to false for the next question.
 
+var correctComments = ['Nice!', 'Great job!', 'Keep it up!']; 
+var incorrectComments = ['Are you drunk?', 'Nope!', 'Way off!']; 
+var correctAudio = new Audio('../TriviaGame/assets/images/bottle3.mp3'); 
+var incorrectAudio = new Audio('../TriviaGame/assets/images/glassBreak.mp3'); 
 
 // console.log(questionArr.q1[0]); --> Displays question
 // var q1 = questionArr.q1[0]; 
-// console.log(questionCycle[0].questionArr[0]); 
+// console.log(questionArr[0]); 
 
 
 
@@ -34,38 +38,107 @@ $(document).ready(function() {
     $('.jumbotron').on('click', '#start', function() {
       console.log('Button clicked!'); 
       $('#start').hide();
+      comment(); 
+      correctAudio.play(); 
+
+      qSet1(); 
+      setTimeout(qSet2, 1000 * 20); 
+      setTimeout(qSet3, 1000 * 35); 
+      setTimeout(qSet4, 1000 * 45); 
+      clearTimeout(qSet4); 
 
       // Round 1 begins
       // Timer Starts counting down
-      timerOn = true; 
-      timer(); 
       // setTimeout() ----> Used to delay an action until the end of the specified time 
       
-      function cycle () {
-        for (i = 0; i < questionCycle.length; i++) {
 
-        }
-
-      function questionSet() {
-        for (i = 0; i < questionArr.length; i++) {
-          setTimeout(questionSet, 1000 * 10); 
           
           
-          $('#questionTag').text(questionArr.q1[i]);
-          // answerChoices for q1 displayed
-    
-          $('#choiceA').text(questionArr.q1[i]);
-          $('#answerChoices').show();
-        }
-      }
-
-
-      // question 1 displayed
-      
-
+          
+          
+          
     }); 
+        
+    function qSet1() {
+      timerOn = true; 
+      timer(); 
+      $('#questionTag').text(questionArr.q1[0]);
+      
+      // answerChoices for q1 displayed
+      $('#answerChoices').show();
+      $('#choiceA').text(questionArr.q1[1]);
+      $('#choiceB').text(questionArr.q1[2]);
+      $('#choiceC').text(questionArr.q1[3]);
+      $('#choiceD').text(questionArr.q1[4]);
+      
+      //-----Better way to cycle through assignment of values??----
+      // for (i = 0; i < questionArr.length; i++) {
+      //   console.log(questionArr.q1[i]); 
+      // }
 
+      // setTimeout(qSet1, 1000 * 10); 
+    }; // END qSet1()
 
+    function qSet2() {
+      timerOn = true; 
+      timer(); 
+      $('#questionTag').text(questionArr.q2[0]);
+      
+      // answerChoices for q1 displayed
+      $('#answerChoices').show();
+      $('#choiceA').text(questionArr.q2[1]);
+      $('#choiceB').text(questionArr.q2[2]);
+      $('#choiceC').text(questionArr.q2[3]);
+      $('#choiceD').text(questionArr.q2[4]);
+      
+      //-----Better way to cycle through assignment of values??----
+      // for (i = 0; i < questionArr.length; i++) {
+      //   console.log(questionArr.q1[i]); 
+      // }
+
+      // setTimeout(qSet1, 1000 * 10); 
+    }; // END qSet2()
+
+    function qSet3() {
+      timerOn = true; 
+      timer(); 
+      $('#questionTag').text(questionArr.q3[0]);
+      
+      // answerChoices for q1 displayed
+      $('#answerChoices').show();
+      $('#choiceA').text(questionArr.q3[1]);
+      $('#choiceB').text(questionArr.q3[2]);
+      $('#choiceC').text(questionArr.q3[3]);
+      $('#choiceD').text(questionArr.q3[4]);
+      
+      //-----Better way to cycle through assignment of values??----
+      // for (i = 0; i < questionArr.length; i++) {
+      //   console.log(questionArr.q1[i]); 
+      // }
+
+      // setTimeout(qSet1, 1000 * 10); 
+    }; // END qSet3()
+
+    function qSet4() {
+      timerOn = true; 
+      timer(); 
+      $('#questionTag').text(questionArr.q4[0]);
+      
+      // answerChoices for q1 displayed
+      $('#answerChoices').show();
+      $('#choiceA').text(questionArr.q4[1]);
+      $('#choiceB').text(questionArr.q4[2]);
+      $('#choiceC').text(questionArr.q4[3]);
+      $('#choiceD').text(questionArr.q4[4]);
+      
+      //-----Better way to cycle through assignment of values??----
+      // for (i = 0; i < questionArr.length; i++) {
+      //   console.log(questionArr.q1[i]); 
+      // }
+
+      // setTimeout(qSet1, 1000 * 10); 
+    }; // END qSet4()
+        
     function timer() {
       if (timerOn == true) {
         $('#timerTag').text(time); 
@@ -86,6 +159,27 @@ $(document).ready(function() {
         return timerOn; 
       }
     }; // END countDown()
+
+    function comment() {
+      var x = Math.floor(Math.random() * 3); 
+      console.log(x); 
+
+      if (correct) {
+        $('#comment').text(correctComments[x]); 
+        $('#comment').animate({opacity: '1'}); 
+        function fadeOut() {
+          $('#comment').animate({opacity: '0'});
+        }
+        setTimeout(fadeOut, 1000 * 2); 
+      } else {
+        $('#comment').text(incorrectComments[x]); 
+        $('#comment').animate({opacity: '1'}); 
+        function fadeOut() {
+          $('#comment').animate({opacity: '0'});
+        }
+        setTimeout(fadeOut, 1000 * 2); 
+      }
+    }
 
 
   } // END gamePlay()
